710d1e2a08c655901517e7cf4d49914a4ffc7952
jusung kim <karlema8900@gmail.com>
2023-09-26 18:08:06 +0900

    [Feat] 간단한 기초 계산기 프로그래밍 완성

---
+++ 1. calculator/1. calculator/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace _1.calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.seven = new System.Windows.Forms.Button();
+            this.calDisplay = new System.Windows.Forms.TextBox();
+            this.eight = new System.Windows.Forms.Button();
+            this.nine = new System.Windows.Forms.Button();
+            this.four = new System.Windows.Forms.Button();
+            this.five = new System.Windows.Forms.Button();
+            this.six = new System.Windows.Forms.Button();
+            this.one = new System.Windows.Forms.Button();
+            this.two = new System.Windows.Forms.Button();
+            this.three = new System.Windows.Forms.Button();
+            this.zero = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+            this.plus = new System.Windows.Forms.Button();
+            this.minus = new System.Windows.Forms.Button();
+            this.equal = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // seven
+            // 
+            this.seven.Font = new System.Drawing.Font("굴림", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.seven.Location = new System.Drawing.Point(50, 136);
+            this.seven.Name = "seven";
+            this.seven.Size = new System.Drawing.Size(70, 68);
+            this.seven.TabIndex = 0;
+            this.seven.Text = "7";
+            this.seven.UseVisualStyleBackColor = true;
+            this.seven.Click += new System.EventHandler(this.seven_Click);
+            // 
+            // calDisplay
+            // 
+            this.calDisplay.Location = new System.Drawing.Point(50, 40);
+            this.calDisplay.Multiline = true;
+            this.calDisplay.Name = "calDisplay";
+            this.calDisplay.Size = new System.Drawing.Size(640, 78);
+            this.calDisplay.TabIndex = 1;
+            // 
+            // eight
+            // 
+            this.eight.Font = new System.Drawing.Font("굴림", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.eight.Location = new System.Drawing.Point(153, 136);
+            this.eight.Name = "eight";
+            this.eight.Size = new System.Drawing.Size(70, 68);
+            this.eight.TabIndex = 0;
+            this.eight.Text = "8";
+            this.eight.UseVisualStyleBackColor = true;
+            this.eight.Click += new System.EventHandler(this.eight_Click);
+            // 
+            // nine
+            // 
+            this.nine.Font = new System.Drawing.Font("굴림", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.nine.Location = new System.Drawing.Point(263, 136);
+            this.nine.Name = "nine";
+            this.nine.Size = new System.Drawing.Size(70, 68);
+            this.nine.TabIndex = 0;
+            this.nine.Text = "9";
+            this.nine.UseVisualStyleBackColor = true;
+            this.nine.Click += new System.EventHandler(this.nine_Click);
+            // 
+            // four
+            // 
+            this.four.Font = new System.Drawing.Font("굴림", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.four.Location = new System.Drawing.Point(50, 224);
+            this.four.Name = "four";
+            this.four.Size = new System.Drawing.Size(70, 68);
+            this.four.TabIndex = 0;
+            this.four.Text = "4";
+            this.four.UseVisualStyleBackColor = true;
+            this.four.Click += new System.EventHandler(this.four_Click);
+            // 
+            // five
+            // 
+            this.five.Font = new System.Drawing.Font("굴림", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.five.Location = new System.Drawing.Point(153, 224);
+            this.five.Name = "five";
+            this.five.Size = new System.Drawing.Size(70, 68);
+            this.five.TabIndex = 0;
+            this.five.Text = "5";
+            this.five.UseVisualStyleBackColor = true;
+            this.five.Click += new System.EventHandler(this.five_Click);
+            // 
+            // six
+            // 
+            this.six.Font = new System.Drawing.Font("굴림", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.six.Location = new System.Drawing.Point(263, 224);
+            this.six.Name = "six";
+            this.six.Size = new System.Drawing.Size(70, 68);
+            this.six.TabIndex = 0;
+            this.six.Text = "6";
+            this.six.UseVisualStyleBackColor = true;
+            this.six.Click += new System.EventHandler(this.six_Click);
+            // 
+            // one
+            // 
+            this.one.Font = new System.Drawing.Font("굴림", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.one.Location = new System.Drawing.Point(50, 310);
+            this.one.Name = "one";
+            this.one.Size = new System.Drawing.Size(70, 68);
+            this.one.TabIndex = 0;
+            this.one.Text = "1";
+            this.one.UseVisualStyleBackColor = true;
+            this.one.Click += new System.EventHandler(this.one_Click);
+            // 
+            // two
+            // 
+            this.two.Font = new System.Drawing.Font("굴림", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.two.Location = new System.Drawing.Point(153, 310);
+            this.two.Name = "two";
+            this.two.Size = new System.Drawing.Size(70, 68);
+            this.two.TabIndex = 0;
+            this.two.Text = "2";
+            this.two.UseVisualStyleBackColor = true;
+            this.two.Click += new System.EventHandler(this.two_Click);
+            // 
+            // three
+            // 
+            this.three.Font = new System.Drawing.Font("굴림", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.three.Location = new System.Drawing.Point(263, 310);
+            this.three.Name = "three";
+            this.three.Size = new System.Drawing.Size(70, 68);
+            this.three.TabIndex = 0;
+            this.three.Text = "3";
+            this.three.UseVisualStyleBackColor = true;
+            this.three.Click += new System.EventHandler(this.three_Click);
+            // 
+            // zero
+            // 
+            this.zero.Font = new System.Drawing.Font("굴림", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.zero.Location = new System.Drawing.Point(375, 310);
+            this.zero.Name = "zero";
+            this.zero.Size = new System.Drawing.Size(315, 68);
+            this.zero.TabIndex = 0;
+            this.zero.Text = "0";
+            this.zero.UseVisualStyleBackColor = true;
+            this.zero.Click += new System.EventHandler(this.zero_Click);
+            // 
+            // clear
+            // 
+            this.clear.Font = new System.Drawing.Font("굴림", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.clear.Location = new System.Drawing.Point(375, 136);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(315, 68);
+            this.clear.TabIndex = 0;
+            this.clear.Text = "AC";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // plus
+            // 
+            this.plus.Font = new System.Drawing.Font("굴림", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.plus.Location = new System.Drawing.Point(375, 224);
+            this.plus.Name = "plus";
+            this.plus.Size = new System.Drawing.Size(70, 68);
+            this.plus.TabIndex = 0;
+            this.plus.Text = "+";
+            this.plus.UseVisualStyleBackColor = true;
+            this.plus.Click += new System.EventHandler(this.plus_Click);
+            // 
+            // minus
+            // 
+            this.minus.Font = new System.Drawing.Font("굴림", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.minus.Location = new System.Drawing.Point(497, 224);
+            this.minus.Name = "minus";
+            this.minus.Size = new System.Drawing.Size(70, 68);
+            this.minus.TabIndex = 0;
+            this.minus.Text = "-";
+            this.minus.UseVisualStyleBackColor = true;
+            this.minus.Click += new System.EventHandler(this.minus_Click);
+            // 
+            // equal
+            // 
+            this.equal.Font = new System.Drawing.Font("굴림", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.equal.Location = new System.Drawing.Point(620, 224);
+            this.equal.Name = "equal";
+            this.equal.Size = new System.Drawing.Size(70, 68);
+            this.equal.TabIndex = 0;
+            this.equal.Text = "=";
+            this.equal.UseVisualStyleBackColor = true;
+            this.equal.Click += new System.EventHandler(this.equal_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(744, 450);
+            this.Controls.Add(this.calDisplay);
+            this.Controls.Add(this.three);
+            this.Controls.Add(this.two);
+            this.Controls.Add(this.equal);
+            this.Controls.Add(this.six);
+            this.Controls.Add(this.minus);
+            this.Controls.Add(this.five);
+            this.Controls.Add(this.one);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.zero);
+            this.Controls.Add(this.nine);
+            this.Controls.Add(this.plus);
+            this.Controls.Add(this.four);
+            this.Controls.Add(this.eight);
+            this.Controls.Add(this.seven);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button seven;
+        private System.Windows.Forms.TextBox calDisplay;
+        private System.Windows.Forms.Button eight;
+        private System.Windows.Forms.Button nine;
+        private System.Windows.Forms.Button four;
+        private System.Windows.Forms.Button five;
+        private System.Windows.Forms.Button six;
+        private System.Windows.Forms.Button one;
+        private System.Windows.Forms.Button two;
+        private System.Windows.Forms.Button three;
+        private System.Windows.Forms.Button zero;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.Button plus;
+        private System.Windows.Forms.Button minus;
+        private System.Windows.Forms.Button equal;
+    }
+}
+
---
+++ 1. calculator/1. calculator/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1.calculator
+{
+    public partial class Form1 : Form
+    {
+
+        bool sign = false;
+        int operatorFlag = 0;
+        int temp = 0;
+        int first = 0;
+        int second = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void one_Click(object sender, EventArgs e)
+        {
+            if(sign == false)
+            {
+                temp = 1;
+                first = 1;
+            }
+            else
+            {
+                second = 1;
+            }
+            calDisplay.Text = "1";
+        }
+
+        private void two_Click(object sender, EventArgs e)
+        {
+            if (sign == false)
+            {
+                temp = 2;
+                first = 2;
+            }
+            else
+            {
+                second = 2;
+            }
+            calDisplay.Text = "2";
+        }
+
+        private void three_Click(object sender, EventArgs e)
+        {
+            if (sign == false)
+            {
+                temp = 3;
+                first = 3;
+            }
+            else
+            {
+                second = 3;
+            }
+            calDisplay.Text = "3";
+        }
+        private void four_Click(object sender, EventArgs e)
+        {
+            if (sign == false)
+            {
+                temp = 4;
+                first = 4;
+            }
+            else
+            {
+                second = 4;
+            }
+            calDisplay.Text = "4";
+        }
+
+        private void five_Click(object sender, EventArgs e)
+        {
+            if (sign == false)
+            {
+                temp = 5;
+                first = 5;
+            }
+            else
+            {
+                second = 5;
+            }
+            calDisplay.Text = "5";
+        }
+
+        private void six_Click(object sender, EventArgs e)
+        {
+            if (sign == false)
+            {
+                temp = 6;
+                first = 6;
+            }
+            else
+            {
+                second = 6;
+            }
+            calDisplay.Text = "6";
+        }
+
+        private void seven_Click(object sender, EventArgs e)
+        {
+            if (sign == false)
+            {
+                temp = 7;
+                first = 7;
+            }
+            else
+            {
+                second = 7;
+            }
+            calDisplay.Text = "7";
+        }
+
+        private void eight_Click(object sender, EventArgs e)
+        {
+            if (sign == false)
+            {
+                temp = 8;
+                first = 8;
+            }
+            else
+            {
+                second = 8;
+            }
+            calDisplay.Text = "8";
+        }
+
+        private void nine_Click(object sender, EventArgs e)
+        {
+            if (sign == false)
+            {
+                temp = 9;
+                first = 9;
+            }
+            else
+            {
+                second = 9;
+            }
+            calDisplay.Text = "9";
+        }
+
+        private void zero_Click(object sender, EventArgs e)
+        {
+            if (sign == false)
+            {
+                first = 0;
+            }
+            else
+            {
+                second = 0;
+            }
+            calDisplay.Text = "0";
+        }
+
+        private void plus_Click(object sender, EventArgs e)
+        {
+            sign = true;
+            operatorFlag = 1;
+            temp += second;
+
+        }
+
+        private void minus_Click(object sender, EventArgs e)
+        {
+            sign = true;
+            operatorFlag = 2;
+            temp -= second;
+        }
+
+        private void equal_Click(object sender, EventArgs e)
+        {
+            
+            
+            int result = 0;
+            if(operatorFlag == 1){
+                result = temp + second;
+            }
+            else if(operatorFlag == 2)
+            {
+                result = temp - second;
+            }
+            temp = result;
+            second = 0;
+            sign = false;
+            operatorFlag = 0;
+            calDisplay.Text = result.ToString();
+
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            first = 0;
+            operatorFlag = 0;
+            second = 0;
+            sign = false;
+            calDisplay.Text = "0";
+        }
+    }
+}
